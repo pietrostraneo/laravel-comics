4e67f788c314fa5c0a0bf96946ac7db71f00a6bd
pietrostraneo <straneo_p@yahoo.it>
2024-02-12 18:44:39 +0100

    Created links array for header's navbar

---
+++ config/link.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    [
+        'label' => 'CHARACTERS'
+    ],
+    [
+        'label' => 'COMICS'
+    ],
+    [
+        'label' => 'MOVIES'
+    ],
+    [
+        'label' => 'TV'
+    ],
+    [
+        'label' => 'GAMES'
+    ],
+    [
+        'label' => 'COLLECTIBLES'
+    ],
+    [
+        'label' => 'VIDEOS'
+    ],
+    [
+        'label' => 'FANS'
+    ],
+    [
+        'label' => 'NEWS'
+    ],
+    [
+        'label' => 'SHOP'
+    ],
+
+];
