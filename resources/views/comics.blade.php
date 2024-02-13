3605ea590504daafdea7faa83bd8ac3229fabe24
pietrostraneo <straneo_p@yahoo.it>
2024-02-13 15:41:56 +0100

    Created layout folder and edited main views files

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,36 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+    @include('partials.jumbo')
 
-    <title>Comics</title>
+    <div class="comics">
+        @include('partials.series')
+    </div>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-    <main>
-
-        <div class="jumbo">
-            <h5>CURRENT SERIES</h5>
-        </div>
-
-        <div class="comics">
-            @include('partials.series')
-        </div>
-
-        @include('partials.banner')
-
-    </main>
-    @include('partials.footer')
-</body>
-
-</html>
+    @include('partials.banner')
+@endsection
